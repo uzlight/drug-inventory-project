eaded599f2373a18d278510149771987b832fe97
Thomas Beerbower <thomas@techelevator.com>
2024-10-02 10:37:42 -0400

    mod 3 week 9 final project

---
+++ client/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="cart-app">
+    <header></header>
+    <nav>
+        <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token">
+          Logout
+        </router-link>
+        <router-link v-bind:to="{ name: 'login' }" v-else>Login</router-link>
+    </nav>
+    <main>
+      <router-view />
+    </main>
+    <footer></footer>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
